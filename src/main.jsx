47841d5144db17356b42ec47baf6b035ce45f73d
yronics-dev <keusricardo@gmail.com>
2026-05-12 23:52:47 +0200

    Fix service worker and registration paths for GitHub Pages subdirectory

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').catch(() => {});
+    navigator.serviceWorker.register('./sw.js').catch(() => {});
   });
 }
 
